d15a34a500fc1ca3af3dede325fc40441f41427b
Florencia-97 <florrr1997@gmail.com>
2017-11-11 23:12:48 -0300

    Agregadas dos pruebas a CarcelTest

--- test/Modelo/Casilleros/CarcelTest.java
+++ test/Modelo/Casilleros/CarcelTest.java
@@ -35,4 +35,29 @@ public class CarcelTest {
         Assert.assertFalse(carcel.permiteSalida(jugador));
     }
 
+    @Test
+    public void testCarcelLiberaAlJugadorLuegoDeDosTurnosDeHaberCaido(){
+        Jugador jugador = new Jugador("batman");
+        Carcel carcel = new Carcel();
+        jugador.caeEn(carcel,0);
+        jugador.empezarTurno();
+        jugador.empezarTurno();
+        jugador.empezarTurno();
+        Assert.assertTrue((carcel.permiteSalida(jugador)));
+    }
+
+    @Test
+    public void testCarcelAceptaAMasDeUnJugador(){
+        Jugador jugador1 = new Jugador("batman");
+        Jugador jugador2 = new Jugador("superman");
+        Carcel carcel = new Carcel();
+        jugador1.caeEn(carcel,0);
+        jugador2.caeEn(carcel,0);
+        jugador1.empezarTurno();
+        jugador2.empezarTurno();
+        Assert.assertTrue(carcel.permiteSalida(jugador1)== carcel.permiteSalida(jugador2));
+    }
+
+
+
 }
