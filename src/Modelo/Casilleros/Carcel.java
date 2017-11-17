3f832850b60588df050fc50374a6270572b63e23
FdelMazo <federicodelmazo@hotmail.com>
2017-11-17 19:44:00 -0300

    Saco out y .iml y los agrego al gitignore

--- src/Modelo/Casilleros/Carcel.java
+++ src/Modelo/Casilleros/Carcel.java
@@ -7,29 +7,9 @@ import Modelo.Preso;
 import java.util.HashMap;
 
 public class Carcel implements Casillero {
-    private HashMap<Jugador, Integer> nochesPresos;
-
-    public Carcel(){
-        nochesPresos = new HashMap<>();
-    }
-
-    private boolean pagarFianza(Jugador jugador) {
-        return jugador.solicitar_dinero(45000);
-    }
 
     public void accionAlCaer(Jugador jugador, int numDado, Tablero tablero) {
         jugador.actualizarEstado(new Preso());
     }
-
-    public boolean permiteSalida(Jugador jugador) {
-        if (nochesPresos.get(jugador) == 1){return false;}
-        else if (nochesPresos.get(jugador) > 3 || this.pagarFianza(jugador)) {
-            nochesPresos.remove(jugador);
-            return true;
-        }
-        return false;
-    }
-
-
 
 }
