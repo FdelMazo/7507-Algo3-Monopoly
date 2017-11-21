7f913fa8688fd9184c69a0ddf04321c5cfaf62dd
FdelMazo <federicodelmazo@hotmail.com>
2017-11-21 00:07:43 -0300

    Agrego handlers de boton comprar y vender.

--- src/Vista/eventos/BotonComprar.java
+++ src/Vista/eventos/BotonComprar.java
@@ -1,8 +1,11 @@
 package Vista.eventos;
 
 import Modelo.Jugador;
+import Vista.Consola;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class BotonComprar implements EventHandler<ActionEvent>{
 
@@ -14,6 +17,11 @@ public class BotonComprar implements EventHandler<ActionEvent>{
 
     @Override
     public void handle(ActionEvent event) {
+        Media musicaInicio = new Media(getClass().getResource("background.mp3").toExternalForm());
+        MediaPlayer mediaPlayer = new MediaPlayer(musicaInicio);
+        mediaPlayer.play();
+        Consola.println("Compro Compro");
         //this.jugador.comprar();
     }
+
 }
--- src/Vista/eventos/BotonVender.java
+++ src/Vista/eventos/BotonVender.java
@@ -1,19 +1,42 @@
 package Vista.eventos;
 
 import Modelo.Jugador;
+import Vista.Consola;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import sun.audio.AudioPlayer;
 
 public class BotonVender implements EventHandler<ActionEvent>{
+    private int contador;
 
     Jugador jugador;
 
     public BotonVender(Jugador jugador){
+        contador = 0;
         this.jugador = jugador;
     }
 
     @Override
     public void handle(ActionEvent event) {
-        //this.jugador.vender();
+        String woah = null;
+        if (contador == 0)woah = "woah0.mp3";
+        else if (contador == 1)woah = "woah1.mp3";
+        else if (contador == 2)woah = "woah2.mp3";
+        else if (contador == 3)woah = "woah3.mp3";
+        else if (contador == 4){
+            contador = 0;
+            woah = "woah4.mp3";
+        };
+
+        Media musicaInicio = new Media(getClass().getResource(woah).toExternalForm());
+        MediaPlayer mediaPlayer = new MediaPlayer(musicaInicio);
+        mediaPlayer.play();
+        Consola.println("Vendo vendo");
+        contador +=1;
     }
 }
+
+
